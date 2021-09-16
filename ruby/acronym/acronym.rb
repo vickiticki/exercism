e27ed9f764f0871907cfdfd0f283bce209720b50
Victoria Tiller <vijotiller@gmail.com>
2021-09-16 11:49:00 -0400

    acronym

---
+++ ruby/acronym/acronym.rb
@@ -0,0 +1,15 @@
+=begin
+Write your code for the 'Acronym' exercise in this file. Make the tests in
+`acronym_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/acronym` directory.
+=end
+
+class Acronym
+  def self.abbreviate(phrase)
+    phrase.gsub!("-", " ")
+    arr = phrase.split
+    arr.map {|word| word[0]}.join.upcase
+end
+end
